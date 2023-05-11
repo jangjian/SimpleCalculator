fa6dedde7a53c75a518328c870031026be5fae19
Jian Jang <pureunnabi@gmail.com>
2023-05-11 18:56:08 +0900

    더하기 기능 구현

--- app/src/main/java/kr/hs/emirim/otani/simplecalculator/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/otani/simplecalculator/MainActivity.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     lateinit var edit1 : EditText;
     lateinit var edit2 : EditText
     lateinit var btnAdd : Button;
-    lateinit var btnSub : Button
-    lateinit var btnMul : Button;
-    lateinit var btnDiv : Button
+    lateinit var btnMinus : Button
+    lateinit var btnMultiply : Button;
+    lateinit var btnDivide : Button
     lateinit var textResult : TextView
     lateinit var num1 : String;
     lateinit var num2 : String
@@ -22,5 +23,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edit1 = findViewById(R.id.edit1)
+        edit2 = findViewById(R.id.edit2)
+        textResult = findViewById(R.id.text_result)
+        btnAdd = findViewById(R.id.btn_plus)
+        btnMinus = findViewById(R.id.btn_minus)
+        btnMultiply = findViewById(R.id.btn_multiply)
+        btnDivide = findViewById(R.id.btn_divide)
+        btnAdd.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) + Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
+
+
+
+
     }
 }
